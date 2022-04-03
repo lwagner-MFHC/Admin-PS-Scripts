b69d7b29f7764a9596eb5e8ca50dc9e4b7149e5c
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 20:50:31 -0600

    Update UninstallInternetExplorer.ps1

--- PowerShellScripts/UninstallInternetExplorer.ps1
+++ PowerShellScripts/UninstallInternetExplorer.ps1
@@ -1,7 +1,9 @@
-﻿.$profile
 $Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
 
-
-.$psexec \\$Machine -u $u -p $p -h powershell -command "Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 –Online -NoRestart"
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+     Disable-WindowsOptionalFeature -FeatureName Internet-Explorer-Optional-amd64 –Online -NoRestart
+     }
 
 Read-Host -Prompt “Press Enter to exit”
