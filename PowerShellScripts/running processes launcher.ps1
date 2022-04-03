69cda57b429d53f49ef169a4ea943276d9825c3d
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:03:02 -0600

    Delete running processes launcher.ps1

--- PowerShellScripts/running processes launcher.ps1
+++
@@ -1,9 +0,0 @@
-﻿.$profile
-$psexec = "\\mfhc-fs01\Share\IT\Useful_Tools\PSTools\PsExec64.exe"
-$Machine = Read-Host "Enter Target Computer Name"
-$Date = Get-Date -UFormat "%A %m-%d-%Y"
-$FileName = "$Machine($Date).csv"
-.$psexec \\$Machine -u $u -p $p -h pwsh -command "& '\\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\running processes.ps1'" 
-
-
-Read-Host -Prompt “Press Enter to exit” 
