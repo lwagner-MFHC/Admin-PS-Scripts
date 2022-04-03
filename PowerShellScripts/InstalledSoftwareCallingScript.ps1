47e0f276298f6863c7938341d817215de38ff547
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:20:13 -0600

    Delete InstalledSoftwareCallingScript.ps1

--- PowerShellScripts/InstalledSoftwareCallingScript.ps1
+++
@@ -1,7 +0,0 @@
-﻿Install-Module ImportExcel -Force
-Import-Module ImportExcel
-$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-$TableDate = Get-Date -UFormat "%a %b %d %Y Time %H%M%S"
-
-& \\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\InstalledSoftware.ps1 | Export-Excel -Path "\\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\csv installed application reports\$Env:Computername.xlsx" -AutoSize -TableName "$TableDate" -WorksheetName "$Date" -BoldTopRow -FreezeTopRow
-
