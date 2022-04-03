ce6507b3d36070141241af530170250964c94f13
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 20:55:55 -0600

    Update gpupdate ps script.ps1

--- PowerShellScripts/gpupdate ps script.ps1
+++ PowerShellScripts/gpupdate ps script.ps1
@@ -1,4 +1,9 @@
-﻿$Machine = Read-Host "Enter Target Computer Name"
-.\psexec \\$Machine -h cmd /C "gpupdate /force"
+$Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
+
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+     gpupdate /force
+     }
 
 Read-Host -Prompt "Press Enter to Exit."
