f7f670c4b2247dafb995ce370fe4864aa8b931b7
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 18:48:43 -0600

    Delete Install TSScan Client.ps1

--- PowerShellScripts/Install TSScan Client.ps1
+++
@@ -1,4 +0,0 @@
-﻿.$profile
-$Machine = Read-Host "Enter Target Computer Name"
-.$psexec \\$Machine -u $u -p $p -h cmd /c "call \\mfhc-fs01\Share\IT\MISWIM\newversions\InstallTSScanClient.bat"
-Read-Host -Prompt “Press Enter to exit”
