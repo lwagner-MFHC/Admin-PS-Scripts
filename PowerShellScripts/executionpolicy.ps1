dad41266c792ee2c679acea44214859eb4d54ff4
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 20:54:00 -0600

    Delete executionpolicy.ps1

--- PowerShellScripts/executionpolicy.ps1
+++
@@ -1,5 +0,0 @@
-.$profile
-$Machine = Read-Host "Enter Target Computer Name"
-.$psexec \\$Machine -u $u -p $p -h powershell -command "Set-ExecutionPolicy unrestricted"
-
-Read-Host -Prompt "Press Enter to exit"
