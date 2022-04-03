35306858349ff52ba749806dee9c1f2ba64ab9d1
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:11:53 -0600

    Delete FileHashing - Advocacy Folder.ps1

--- PowerShellScripts/FileHashing - Advocacy Folder.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Module ImportExcel
-$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-$TableDate = Get-Date -UFormat "%a, %b %d, %Y Time %H_%M_%S"
-
-Get-ChildItem -Path "\\mfhc-fs01\Share\Advocacy" -Recurse -File | Get-FileHash  | Export-Excel -Path "\\mfhc-fs01\Users\lwagner.MFHC\lwagner\Auditing\Advocacy Folder\Advocacy Folder FileHashes.xlsx" -AutoSize -TableName "$TableDate" -WorksheetName "$Date" -BoldTopRow -FreezeTopRow -Show
