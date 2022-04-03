0d1c70e6ae758fea4d728f58a01737f7531bad05
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 19:20:22 -0600

    Update PrinterAdding.ps1

--- PowerShellScripts/PrinterAdding.ps1
+++ PowerShellScripts/PrinterAdding.ps1
@@ -1,6 +1,9 @@
-﻿.$profile
+﻿$Printer = Read-Host "Enter Path of Desired Printer Name"
 $Machine = Read-Host "Enter Target Computer Name"
-$Printer = Read-Host "Enter Path of Desired Printer Name"
-.$psexec \\$Machine -u $u -p $p -h powershell -command "Add-Printer -ConnectionName '\\mfhc-print01\$Printer'; Get-Printer"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+     Add-Printer -ConnectionName '<print server>\$Printer'
+     }
 
 Read-Host -Prompt “Press Enter to exit”
