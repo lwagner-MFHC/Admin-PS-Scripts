1351885a2b53818958f174e733314542243d8c69
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 19:05:26 -0600

    Update Install Dot NET.ps1

--- PowerShellScripts/Install Dot NET.ps1
+++ PowerShellScripts/Install Dot NET.ps1
@@ -1,4 +1,6 @@
 $Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
 
 Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {<path to Dot Net installer>\ndp48-x86-x64-allos-enu.exe /q /norestart}
 
