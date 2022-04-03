32f5d1ef15dc0bef20a9600f0e2a470a9e971223
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 20:44:25 -0600

    Update SetDefaultPrinter.ps1

--- PowerShellScripts/SetDefaultPrinter.ps1
+++ PowerShellScripts/SetDefaultPrinter.ps1
@@ -1,7 +1,9 @@
-﻿.$profile
-$Machine = Read-Host "Enter Target Computer Name"
-.$psexec \\$Machine -u $u -p $p powershell -command "Get-Printer"
 $Printer = Read-Host "Enter Desired Pritner Name"
-.$psexec \\$Machine -u $u -p $p cmd /c "CScript C:\Windows\System32\Printing_Admin_Scripts\en-US\prnmngr.vbs -t -p $Printer"
+$Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+     CScript C:\Windows\System32\Printing_Admin_Scripts\en-US\prnmngr.vbs -t -p $Printer
+     }
 
 Read-Host -Prompt “Press Enter to exit”
