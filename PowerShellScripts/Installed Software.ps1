c1a05f9cab9f8f7c2356b0ce76ee48addca98ccc
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:19:40 -0600

    Delete Installed Software.ps1

--- PowerShellScripts/Installed Software.ps1
+++
@@ -1,6 +0,0 @@
-﻿.$profile
-$Machine = Read-Host "Enter Target Computer Name"
-$Date = Get-Date -UFormat "%A %m-%d-%Y"
-$FileName = "$Machine($Date).csv"
-.$psexec \\$Machine -u $u -p $p -h pwsh -command "& \\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\InstalledSoftwareCallingScript.ps1"
-start "\\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\csv installed application reports\$Machine.xlsx"
