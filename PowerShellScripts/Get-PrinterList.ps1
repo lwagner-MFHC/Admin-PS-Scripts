e5e59493bffac46511372e1b418f2151ed4a8c91
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 19:08:08 -0600

    Update Get-PrinterList.ps1

--- PowerShellScripts/Get-PrinterList.ps1
+++ PowerShellScripts/Get-PrinterList.ps1
@@ -1,4 +1,10 @@
-Install-Module ImportExcel -Force
-Import-Module ImportExcel
-$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-Get-Printer | Select-Object -Property Name, PrinterStatus, DriverName, PortName | Export-Excel -Path "<path to folder for output file>\$Env:Computername.xlsx" -AutoSize -TableName "$Env:Computername_Printers" -WorksheetName "$Date"
+$Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
+
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+     Install-Module ImportExcel -Force
+     Import-Module ImportExcel
+     $Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
+     Get-Printer | Select-Object -Property Name, PrinterStatus, DriverName, PortName | Export-Excel -Path "<path to folder for output file>\$Env:Computername.xlsx" -AutoSize -TableName "$Env:Computername_Printers" -WorksheetName "$Date"
+     }
