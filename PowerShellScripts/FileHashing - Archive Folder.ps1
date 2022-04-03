8a26b196fc5b52b74952513281b3ad355d561503
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:12:03 -0600

    Delete FileHashing - Archive Folder.ps1

--- PowerShellScripts/FileHashing - Archive Folder.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Module ImportExcel
-$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-$TableDate = Get-Date -UFormat "%a, %b %d, %Y Time %H_%M_%S"
-
-Get-ChildItem -Path "\\mfhc-fs01\Share\Archive" -Recurse -File | Get-FileHash  | Export-Excel -Path "\\mfhc-fs01\Users\lwagner.MFHC\lwagner\Auditing\Archive Folder\Archive Folder FileHashes.xlsx" -AutoSize -TableName "$TableDate" -WorksheetName "$Date" -BoldTopRow -FreezeTopRow -Show
