804079ff98344591be4290ae638f76d09810ebc4
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:15:11 -0600

    Update FindLoggedOnUserSessions.ps1

--- PowerShellScripts/FindLoggedOnUserSessions.ps1
+++ PowerShellScripts/FindLoggedOnUserSessions.ps1
@@ -1,5 +1,4 @@
-﻿.$profile
-$usernm = Read-Host "Enter Target Username"
-\\mfhc-fs01\Share\IT\Useful_Tools\PSTools\PsLoggedon64.exe /accepteula $usernm
+﻿$usernm = Read-Host "Enter Target Username"
+<path to psloggedon program>\PsLoggedon64.exe /accepteula $usernm
 
 Read-Host -Prompt “Press Enter to exit”
