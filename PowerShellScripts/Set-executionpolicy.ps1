da4edb4eee65a620c057bd3151b976ca279cee41
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 19:16:40 -0600

    Update and rename PDQexecutionpolicy.ps1 to Set-executionpolicy.ps1

---
+++ PowerShellScripts/Set-executionpolicy.ps1
@@ -0,0 +1,6 @@
+$Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+      Set-ExecutionPolicy unrestricted
+}
