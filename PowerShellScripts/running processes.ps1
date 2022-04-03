fb724b9c880f4821a3de588a84a0c5e6155a2bab
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:06:17 -0600

    Update running processes.ps1

--- PowerShellScripts/running processes.ps1
+++ PowerShellScripts/running processes.ps1
@@ -1,8 +1,12 @@
-﻿Install-Module ImportExcel -Force
-Import-Module ImportExcel
-$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-$Computername = Get-ChildItem Env:Computername
-$FileName = "$env:computername.xlsx"
-cd \\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\csv_running_processes_reports; Get-Process; Get-Process | Export-Excel -Path "\\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\csv_running_processes_reports\$FileName" -AutoSize -TableName $Env:Computername_Processes -WorksheetName "$Date"
-
+﻿$Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+     Install-Module ImportExcel -Force
+     Import-Module ImportExcel
+     $Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
+     $Computername = Get-ChildItem Env:Computername
+     $FileName = "$env:computername.xlsx"
+     cd <path to desired output folder>; Get-Process; Get-Process | Export-Excel -Path "<path to desired output folder>\$FileName" -AutoSize -TableName $Env:Computername_Processes -WorksheetName "$Date"
+     }
 exit 
