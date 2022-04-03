8148558c1a0a94da1ac8238d7bd92aa5d73143dc
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 19:14:11 -0600

    Update O365 Quick Repair 64-bit.ps1

--- PowerShellScripts/O365 Quick Repair 64-bit.ps1
+++ PowerShellScripts/O365 Quick Repair 64-bit.ps1
@@ -1,7 +1,10 @@
-﻿.$profile
 $Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
 
-
-.$psexec \\$Machine -u $u -p $p -h powershell -command "cd 'C:\Program Files\Microsoft Office 15\ClientX64\' ; .\OfficeClickToRun.exe scenario=Repair DisplayLevel=false RepairType=QuickRepair forceappshutdown=true"
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock { 
+     cd 'C:\Program Files\Microsoft Office 15\ClientX64\' 
+     .\OfficeClickToRun.exe scenario=Repair DisplayLevel=false RepairType=QuickRepair forceappshutdown=true
+     }
 
 Read-Host -Prompt “Press Enter to exit”
