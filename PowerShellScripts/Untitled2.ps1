7a806b39590af3e552582f990cc33b69c7c1a37a
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:47:55 -0600

    Delete Untitled2.ps1

--- PowerShellScripts/Untitled2.ps1
+++
@@ -1,3 +0,0 @@
-﻿cd "\\mfhc-fs01\Share\IT\Useful_Tools\PSTools\Scripts\csv installed application reports"; Get-Process | Export-Csv '$FileName'; Import-Csv '$FileName' | Select-Object -Property Name, Id, Path | Format-Table *
-
-exit 
