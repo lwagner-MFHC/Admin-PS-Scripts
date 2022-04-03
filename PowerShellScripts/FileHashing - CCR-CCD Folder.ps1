0ee0648129da8bf53e4f3c444e759c55946e16ac
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:12:22 -0600

    Delete FileHashing - CCR-CCD Folder.ps1

--- PowerShellScripts/FileHashing - CCR-CCD Folder.ps1
+++
@@ -1,6 +0,0 @@
-﻿Import-Module ImportExcel
-$Date = Get-Date -UFormat "%a, %b %d, %Y Time %H.%M.%S"
-$TableDate = Get-Date -UFormat "%a, %b %d, %Y Time %H_%M_%S"
-$Location = "CCR-CCD"
-
-Get-ChildItem -Path "\\mfhc-fs01\Share\$Location" -Recurse -File | Get-FileHash | Export-Excel -Path "\\mfhc-fs01\Users\lwagner.MFHC\lwagner\Auditing\$Location Folder\$Location Folder FileHashes.xlsx" -AutoSize -TableName "$TableDate" -WorksheetName "$Date" -BoldTopRow -FreezeTopRow -Show
