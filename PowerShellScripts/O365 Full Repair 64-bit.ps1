f3c89ffc7a7795afad5cd2018783cf95765300b9
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 19:12:08 -0600

    Update O365 Full Repair 64-bit.ps1

--- PowerShellScripts/O365 Full Repair 64-bit.ps1
+++ PowerShellScripts/O365 Full Repair 64-bit.ps1
@@ -1,7 +1,10 @@
-﻿.$profile
 $Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
 
-
-.$psexec \\$Machine -u $u -p $p -h powershell -command "cd 'C:\Program Files\Microsoft Office 15\ClientX64\' ; .\OfficeClickToRun.exe scenario=Repair DisplayLevel=false RepairType=FullRepair forceappshutdown=true"
+     cd 'C:\Program Files\Microsoft Office 15\ClientX64\' 
+     .\OfficeClickToRun.exe scenario=Repair DisplayLevel=false RepairType=FullRepair forceappshutdown=true
+     }
 
 Read-Host -Prompt “Press Enter to exit”
