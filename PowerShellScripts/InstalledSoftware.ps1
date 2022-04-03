51f0e0b0902bd89ce8fd28ead54917c845f53798
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 19:01:02 -0600

    Update InstalledSoftware.ps1

--- PowerShellScripts/InstalledSoftware.ps1
+++ PowerShellScripts/InstalledSoftware.ps1
@@ -1,10 +1,18 @@
-﻿$paths=@(
-  'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\',
-  'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
-)
-foreach($path in $paths){
-  Get-ChildItem -Path $path | 
-    Get-ItemProperty | 
-      Select DisplayName, Publisher, InstallDate, DisplayVersion, UninstallString 
-}
+﻿$Machine = Read-Host "Enter Target Computer Name"
+$Domain = Read-Host "Enter relevant domain for user logon"
+$Username = Read-Host "Enter username with required privileges for command execution" 
 
+Invoke-Command -ComputerName $Machine -Credential <domain>\<username to run command as> -ScriptBlock {
+     Install-Module ImportExcel -Force
+     Import-Module ImportExcel
+
+     $paths=@(
+       'HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\',
+       'HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\'
+     )
+     foreach($path in $paths){
+       Get-ChildItem -Path $path | 
+         Get-ItemProperty | 
+           Select DisplayName, Publisher, InstallDate, DisplayVersion, UninstallString 
+     } | Export-Excel -Path "<path to folder for script output>\$Env:Computername.xlsx" -AutoSize -TableName "$Env:Computername_Software" -WorksheetName "$Date" -BoldTopRow -FreezeTopRow -Show
+}
