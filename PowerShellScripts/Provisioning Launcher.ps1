5e20f82f5c012e12be80d4f044725c183ef5ed7d
lwagner-MFHC <82891644+lwagner-MFHC@users.noreply.github.com>
2022-04-02 21:11:01 -0600

    Delete Provisioning Launcher.ps1

--- PowerShellScripts/Provisioning Launcher.ps1
+++
@@ -1,5 +0,0 @@
-.$profile
-$Machine = Read-Host "Enter Target Computer Name"
-.$psexec \\$Machine -u $u -p $p -h cmd /c "call \\mfhc-fs01\Share\IT\MISWIM\callprovisioningscript.bat"
-
-Read-Host -Prompt “Press Enter to exit” 
